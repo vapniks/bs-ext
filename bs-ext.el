7d51b8868b0f34fe5c43129fde2eb9618499168d
Joe Bloggs <vapniks@yahoo.com>
2013-05-12 00:36:35 +0100

    Bump version to 0.2

--- bs-ext.el
+++ bs-ext.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Joe Bloggs <vapniks@yahoo.com>
 ;; Copyleft (Ↄ) 2012, Joe Bloggs, all rites reversed.
 ;; Created: 2012-06-28 14:23:56
-;; Version: 0.1
+;; Version: 0.2
 ;; Last-Updated: 2012-06-28 14:23:56
 ;;           By: Joe Bloggs
 ;; URL: http://www.emacswiki.org/emacs/download/bs-ext.el
